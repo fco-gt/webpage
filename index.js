9e55172c5806896965596fe93f1e4dc4d4536692
Mankeke <franomg10@gmail.com>
2022-03-07 22:24:59 -0300

    Portafolio

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Settings
+app.set('port', 3000)
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+// Routes
+app.use(require('./routes/index'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Listening server
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+})
